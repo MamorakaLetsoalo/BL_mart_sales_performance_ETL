d52f56a0cac71bbbf9c649deade45b3bc2aeea07
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-07-11 12:31:28 +0200

    Add scripts folder

---
+++ scripts/01 create database.sql
@@ -0,0 +1,16 @@
+--create database 'dwh_BL_mart'
+
+CREATE DATABASE dwh_BL_mart
+GO
+
+USE dwh_BL_mart
+
+--create schemas
+
+CREATE SCHEMA bronze
+
+GO
+CREATE SCHEMA silver
+
+GO
+CREATE SCHEMA gold
