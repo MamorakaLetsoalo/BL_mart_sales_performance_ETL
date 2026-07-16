cae9fa327512c294111ea26e7b5c2eafad124d9e
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-07-16 20:19:56 +0200

    create dim inventory table

---
+++ scripts/gold_layer/004 dim_inventory.sql
@@ -0,0 +1,10 @@
+--create inventory dimension
+
+CREATE TABLE gold.dim_inventory
+(
+    inventory_key INT IDENTITY(1,1) PRIMARY KEY,
+    sku VARCHAR(50),
+    stock_on_hand INT,
+    reorder_threshold INT,
+    created_at DATETIME DEFAULT GETDATE()
+);
