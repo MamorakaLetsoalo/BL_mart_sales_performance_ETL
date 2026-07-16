31789e39cfb465ceaba85c8f9954d676e59d3ee1
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-07-16 20:07:21 +0200

    create dim customer table

---
+++ scripts/gold_layer/001 dim_customer.sql
@@ -0,0 +1,15 @@
+--Create customer dimension tabele
+
+CREATE TABLE gold.dim_customer
+(
+    customer_key INT IDENTITY(1,1) PRIMARY KEY,
+    customer_email VARCHAR(255),
+    customer_first_name VARCHAR(100),
+    customer_last_name VARCHAR(100),
+    customer_phone VARCHAR(30),
+    customer_city VARCHAR(100),
+    customer_province VARCHAR(100),
+    customer_loyalty_tier VARCHAR(50),
+    customer_since DATE,
+    created_at DATETIME DEFAULT GETDATE()
+);
